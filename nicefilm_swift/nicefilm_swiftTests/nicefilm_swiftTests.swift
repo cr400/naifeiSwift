54631414f1e67bb9a19498d94b4e5238ca02f495
yfq <msicr400@163.com>
2018-01-23 15:31:27 +0800

    项目搭建

---
+++ nicefilm_swift/nicefilm_swiftTests/nicefilm_swiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  nicefilm_swiftTests.swift
+//  nicefilm_swiftTests
+//
+//  Created by admin on 2018/1/23.
+//  Copyright © 2018年 yfq. All rights reserved.
+//
+
+import XCTest
+@testable import nicefilm_swift
+
+class nicefilm_swiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
